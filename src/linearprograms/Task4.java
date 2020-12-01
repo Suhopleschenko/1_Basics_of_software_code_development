54c8d8d8ead87afcd74265b83c12faf51b80fdb9
Suhopleshenko <aleksey.suhopleschenko@gmail.com>
2020-12-01 21:35:29 +0300

    task4

---
+++ src/linearprograms/Task4.java
@@ -0,0 +1,17 @@
+package linearprograms;
+
+import java.util.Scanner;
+
+import static java.lang.Math.round;
+
+public class Task4 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Введите число вида nnn.ddd: ");
+        double a = scan.nextDouble();
+        double b = round(a);
+        double c = round((a - b) * 1000);
+        double d = c + b / 1000;
+        System.out.println("Ваше измененное число: " + d);
+    }
+}
